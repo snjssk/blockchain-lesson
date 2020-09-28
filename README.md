d3346dea6fde647fb54ca51f5a36cd3be29cae66
snjssk <snjssk0403@gmail.com>
2020-09-28 23:40:59 +0900

    create blocks

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # blockchain-lesson
+こちらのコースの学習用リポジトリ
+
+- 現役シリコンバレーエンジニアが教えるPythonで始めるスクラッチからのブロックチェーン開発入門
+- https://www.udemy.com/course/python-blockchain/
--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
+import hashlib
+import json
 import logging
 import sys
 import time
+
+import utils
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
@@ -10,20 +14,26 @@ class BlockChain(object):
         self.transcction_pool = []
         self.chain = []
         #初期を作成
-        self.create_block(0, 'init hash')
+        self.create_block(0, self.hash({}))
 
     # blockの作成
     def create_block(self, nonce, previous_hash):
-        block = {
+        # block作成時、キーの並び順を揃えてあげる
+        block = utils.sorted_dict_by_key({
             'timestamp': time.time(),
             'transactions': self.transcction_pool,
             'nonce': nonce,
             'previous_hash': previous_hash
-        }
+        })
         self.chain.append(block)
         self.transcction_pool = []
         return block
 
+    def hash(self, block):
+        # sortの二重チェック
+        # 文字列にするときにjson.dumps(xx, sort_keys=True)とするとソートしてくれる
+        sorted_block = json.dumps(block, sort_keys=True)
+        return hashlib.sha256(sorted_block.encode()).hexdigest()
 
 def pprint(chains):
     for i, chain in enumerate(chains):
@@ -35,8 +45,13 @@ def pprint(chains):
 if __name__ == '__main__':
     block_chain = BlockChain()
     pprint(block_chain.chain)
-    block_chain.create_block(1, 'hash 1')
-    block_chain.create_block(2, 'hash 2')
+
+    previous_hash = block_chain.hash(block_chain.chain[-1])
+    block_chain.create_block(1, previous_hash)
+
+    previous_hash = block_chain.hash(block_chain.chain[-1])
+    block_chain.create_block(2, previous_hash)
+
     pprint(block_chain.chain)
 
 
