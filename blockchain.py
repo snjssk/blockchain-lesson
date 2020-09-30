a7ed60eb6d4137c53aedc49ae08059f51869d99a
snjssk <snjssk0403@gmail.com>
2020-09-30 23:54:23 +0900

    update

--- blockchain.py
+++ blockchain.py
@@ -7,28 +7,32 @@ import time
 import utils
 
 MINING_DIFFICULTY = 3
+MINING_SENDER = 'THE BLOCK CHAIN'
+MINING_REWARD = 0.1
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
 
 class BlockChain(object):
 
-    def __init__(self):
-        self.transcction_pool = []
+    def __init__(self, blockchain_address):
+        self.transaction_pool = []
         self.chain = []
         #初期を作成
         self.create_block(0, self.hash({}))
+        self.blockchain_address = blockchain_address
 
     # blockの作成
     def create_block(self, nonce, previous_hash):
         # block作成時、キーの並び順を揃えてあげる
         block = utils.sorted_dict_by_key({
             'timestamp': time.time(),
-            'transactions': self.transcction_pool,
+            'transactions': self.transaction_pool,
             'nonce': nonce,
             'previous_hash': previous_hash
         })
         self.chain.append(block)
-        self.transcction_pool = []
+        self.transaction_pool = []
         return block
 
     # ハッシュを作る
@@ -47,32 +51,62 @@ class BlockChain(object):
             'recipient_blockchain_address': recipient_blockchain_address,
             'value': float(value)
         })
-        self.transcction_pool.append(transaction)
+        self.transaction_pool.append(transaction)
         return True
 
     # コンセンサスアルゴリズムの計算
     def valid_proof(self, transactions, previous_hash, nonce,
                     difficulty=MINING_DIFFICULTY):
+        # ブロックをつくって
+        guess_block = utils.sorted_dict_by_key({
+            'transactions': transactions,
+            'previous_hash': previous_hash,
+            'nonce': nonce
+        })
+        # ハッシュを作って
+        guess_hash = self.hash(guess_block)
+        # 判定をする
+        return guess_hash[:difficulty] == '0'*difficulty
 
     # ナンスの作成
     def proof_of_work(self):
         # 値が書き換わってしまうのでコピーをつかう
-        transactions = self.transcction_pool.copy()
+        transactions = self.transaction_pool.copy()
         previous_hash = self.hash(self.chain[-1])
         nonce = 0
+        while self.valid_proof(transactions, previous_hash, nonce) is False:
+            nonce += 1
+        return nonce
+
+    # マイニング
+    def mining(self):
+        self.add_transaction(
+            sender_blockchain_address=MINING_SENDER,
+            recipient_blockchain_address=self.blockchain_address,
+            value=MINING_REWARD)
+        nonce = self.proof_of_work()
+        previous_hash = self.hash(self.chain[-1])
+        self.create_block(nonce, previous_hash)
+        logging.info({'action': 'minig', 'status': 'success'})
+        return True
 
 
 if __name__ == '__main__':
-    block_chain = BlockChain()
+    my_blockchain_address = 'my_blockchain_address'
+    block_chain = BlockChain(blockchain_address=my_blockchain_address)
     # pprint(block_chain.chain)
 
     block_chain.add_transaction('A', 'B', 1.0)
-    previous_hash = block_chain.hash(block_chain.chain[-1])
-    block_chain.create_block(1, previous_hash)
+    block_chain.mining()
+    # previous_hash = block_chain.hash(block_chain.chain[-1])
+    # nonce = block_chain.proof_of_work()
+    # block_chain.create_block(nonce, previous_hash)
 
     block_chain.add_transaction('C', 'D', 2.0)
-    previous_hash = block_chain.hash(block_chain.chain[-1])
-    block_chain.create_block(2, previous_hash)
+    block_chain.mining()
+    # previous_hash = block_chain.hash(block_chain.chain[-1])
+    # nonce = block_chain.proof_of_work()
+    # block_chain.create_block(nonce, previous_hash)
 
     utils.pprint(block_chain.chain)
 
