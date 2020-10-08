ccc527503b006a07c01df6f2302aa84fc87e2e7f
snjssk <snjssk0403@gmail.com>
2020-10-08 22:09:36 +0900

    update

--- wallet.py
+++ wallet.py
@@ -5,6 +5,8 @@ import hashlib
 from ecdsa import NIST256p
 from ecdsa import SigningKey
 
+import utils
+
 class Wallet(object):
 
     def __init__(self):
@@ -28,7 +30,9 @@ class Wallet(object):
     def generate_blockchain_address(self):
         # 2 sha256で作成
         public_key_bytes = self._private_key.to_string()
+        # binary public key
         sha256_bpk = hashlib.sha256(public_key_bytes)
+        # hash
         sha256_bpk_digest = sha256_bpk.digest()
 
         # 3 sha256→ripemd ripemdはhashよりも短いハッシュを生成できる
@@ -59,9 +63,43 @@ class Wallet(object):
         blockchain_address = base58.b58encode(address_hex).decode('utf-8')
         return blockchain_address
 
+class Transaction(object):
+
+    def __init__(self, sender_private_key, sender_public_key,
+                 sender_blockchain_address, recipient_blockchain_address,
+                 value):
+        self.sender_private_key = sender_private_key
+        self.sender_public_key = sender_public_key
+        self.sender_blockchain_address = sender_blockchain_address
+        self.recipient_blockchain_address = recipient_blockchain_address
+        self.value = value
+
+    # シグネチャの作成
+    def generate_signature(self):
+        sha256 = hashlib.sha256()
+        transaction = utils.sorted_dict_by_key({
+            'sender_blockchain_address': self.sender_blockchain_address,
+            'recipient_blockchain_address': self.recipient_blockchain_address,
+            'value': float(self.value)
+        })
+        sha256.update(str(transaction).encode('utf-8'))
+        message = sha256.digest()
+        # プライベートキーのオブジェクト
+        private_key = SigningKey.from_string(
+            bytes().fromhex(self.sender_private_key), curve=NIST256p)
+
+        private_key_sign = private_key.sign(message)
+        signature = private_key_sign.hex()
+        return signature
 
 if __name__ == '__main__':
     wallet = Wallet()
     print(wallet.private_key)
     print(wallet.public_key)
     print(wallet.blockchain_address)
+
+    t = Transaction(
+        wallet.private_key, wallet.public_key, wallet.blockchain_address,
+        'B', 2.0
+    )
+    print(t.generate_signature())
