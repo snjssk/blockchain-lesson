0eb26b99bede5da1fc4ada474e4e758589de2106
snjssk <snjssk0403@gmail.com>
2020-10-30 23:36:58 +0900

    update

--- wallet_server.py
+++ wallet_server.py
@@ -4,4 +4,34 @@ from flask import render_template
 
 import wallet
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
+
+# top
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+# postで返す
+@app.route('/wallet', methods=['POST'])
+def create_wallet():
+    my_wallet = wallet.Wallet()
+    response = {
+        'private_key': my_wallet.private_key,
+        'public_key': my_wallet.public_key,
+        'blockchain_address': my_wallet.blockchain_address,
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=8080,
+                        type=int, help='port to liston on')
+    parser.add_argument('-g', '--gw', default='http://127.0.0.1:5000',
+                        type=str, help='bookchain gateway')
+    args = parser.parse_args()
+    port = args.port
+
+    app.config['gw'] = args.gw
+
+    app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
